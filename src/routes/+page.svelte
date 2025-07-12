471b4ce91e420379806ab4112859474b82841d8f
Christian Smith <christianryansmith@gmail.com>
2025-07-12 14:37:19 -0500

    feat: basic shadcn-svelte working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/core";
+  import { Button } from "$lib/components/ui/button/index.js";
 
   let name = $state("");
   let greetMsg = $state("");
@@ -11,12 +12,13 @@
   }
 </script>
 
-<main class="bg-neutral-600 flex flex-col justify-center items-center w-screen h-screen">
+<main class="flex flex-col justify-center items-center w-screen h-screen">
   <h1 class="text-xl text-red-400 font-bold">Welcome to Tauri + Svelte</h1>
   <form class="row" onsubmit={greet}>
-    <input id="greet-input" placeholder="Enter a name..." bind:value={name} class="border-1 border-blue-200 rounded-lg p-2 text-gray-100"/>
+    <input id="greet-input" placeholder="Enter a name..." bind:value={name} class="border-1 border-blue-200 rounded-lg p-2"/>
     <button type="submit">Greet</button>
   </form>
   <p class="text-xl">{greetMsg}</p>
+  <Button>Click</Button>
 </main>
 
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from '@tailwindcss/vite';
+import path from "path";
 
 // @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
@@ -28,6 +29,11 @@ export default defineConfig(async () => ({
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
       ignored: ["**/src-tauri/**"],
+    },
+  },
+  resolve: {
+    alias: {
+      $lib: path.resolve("./src/lib"),
     },
   },
 }));
