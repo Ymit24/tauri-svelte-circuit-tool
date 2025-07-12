6dfeb666f4aff13e1b91e5589900633a2ce2c0e6
Christian Smith <christianryansmith@gmail.com>
2025-07-12 16:33:17 -0500

    wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,4 @@
 <script lang="ts">
-  import { invoke } from "@tauri-apps/api/core";
-  import { Button } from "$lib/components/ui/button/index.js";
-  import { Input } from "$lib/components/ui/input/index.js";
-
-  let name = $state("");
-  let greetMsg = $state("");
-
-  async function greet(event: Event) {
-    event.preventDefault();
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsg = await invoke("greet", { name });
-  }
 </script>
 
-<main class="flex flex-col justify-center items-center flex-1 gap-2">
-  <h1
-    class="text-xl text-red-400 font-bold
-    animate-in fade-in zoom-in duration-100"
-  >
-    Welcome to Tauri + Svelte
-  </h1>
-  <form class="flex flex-row gap-4" onsubmit={greet}>
-    <Input placeholder="Enter a name..." bind:value={name} />
-    <Button type="submit">Greet</Button>
-  </form>
-</main>
+<main class="flex flex-col justify-center items-center flex-1 gap-2"></main>
