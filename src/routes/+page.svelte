b247089d602138681eeca6d1a61fb8f64e6a4bc3
Christian Smith <christianryansmith@gmail.com>
2025-07-12 16:37:08 -0500

    wip: got basic svelvet working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
 <script lang="ts">
+  import { Svelvet, Node, Minimap, Controls, ThemeToggle } from "svelvet";
 </script>
 
-<main class="flex flex-col justify-center items-center flex-1 gap-2"></main>
+<main class="flex flex-col justify-center flex-1 gap-2 h-full">
+  <h1 class="text-4xl font-bold">Circuit</h1>
+  <Svelvet id="my-canvas" TD minimap controls locked>
+    <Node />
+    <Node id="alpha" bgColor="red" label="Default Node" />
+    <ThemeToggle main="dark" alt="light" slot="toggle" />
+  </Svelvet>
+</main>
